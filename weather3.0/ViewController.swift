04c5d6347077cba3f09cf6c151b471828787e40d
Alexandr Sabri <s@thenine.co>
2017-06-09 20:06:00 +0300

    Project initial

---
+++ weather3.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather3.0
+//
+//  Created by Александр Сабри on 09.06.17.
+//  Copyright © 2017 sabry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
