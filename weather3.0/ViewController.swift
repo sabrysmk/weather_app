3affbdbc28ab4db7dfd09df776af0bf82a6c35d1
Alexandr Sabri <s@thenine.co>
2017-06-09 22:56:34 +0300

    Add main UI

--- weather3.0/ViewController.swift
+++ weather3.0/ViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var datelbl: UILabel!
+    @IBOutlet weak var templbl: UILabel!
+    @IBOutlet weak var citylbl: UILabel!
+    @IBOutlet weak var typelbl: UILabel!
+    @IBOutlet weak var weatherimage: UIImageView!
+    
+  
+    @IBOutlet weak var tableview: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weathercell", for: indexPath)
+        
+        return cell
     }
 
 
